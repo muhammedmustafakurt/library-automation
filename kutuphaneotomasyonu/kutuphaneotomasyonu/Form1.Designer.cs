ad2259b5560f78e00a0eccd88fcf4ce02a9f4706
sasia2701 <148706805+sasia2701@users.noreply.github.com>
2024-12-19 04:19:54 +0300

    a

---
+++ kutuphaneotomasyonu/kutuphaneotomasyonu/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace kutuphaneotomasyonu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabPage3 = new System.Windows.Forms.TabPage();
+            label7 = new System.Windows.Forms.Label();
+            label6 = new System.Windows.Forms.Label();
+            textBox7 = new System.Windows.Forms.TextBox();
+            textBox6 = new System.Windows.Forms.TextBox();
+            button3 = new System.Windows.Forms.Button();
+            tabPage2 = new System.Windows.Forms.TabPage();
+            textBox1 = new System.Windows.Forms.TextBox();
+            label1 = new System.Windows.Forms.Label();
+            button1 = new System.Windows.Forms.Button();
+            tabPage1 = new System.Windows.Forms.TabPage();
+            label5 = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            textBox4 = new System.Windows.Forms.TextBox();
+            textBox5 = new System.Windows.Forms.TextBox();
+            textBox3 = new System.Windows.Forms.TextBox();
+            textBox2 = new System.Windows.Forms.TextBox();
+            button2 = new System.Windows.Forms.Button();
+            tabControl1 = new System.Windows.Forms.TabControl();
+            tabPage3.SuspendLayout();
+            tabPage2.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabControl1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabPage3
+            // 
+            tabPage3.Controls.Add(label7);
+            tabPage3.Controls.Add(label6);
+            tabPage3.Controls.Add(textBox7);
+            tabPage3.Controls.Add(textBox6);
+            tabPage3.Controls.Add(button3);
+            tabPage3.Location = new System.Drawing.Point(4, 29);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            tabPage3.Size = new System.Drawing.Size(1098, 410);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Ödünç Ver";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new System.Drawing.Point(248, 41);
+            label7.Name = "label7";
+            label7.Size = new System.Drawing.Size(71, 20);
+            label7.TabIndex = 4;
+            label7.Text = "Kitap Adı";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new System.Drawing.Point(35, 41);
+            label6.Name = "label6";
+            label6.Size = new System.Drawing.Size(62, 20);
+            label6.TabIndex = 3;
+            label6.Text = "Numara";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new System.Drawing.Point(248, 64);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new System.Drawing.Size(180, 27);
+            textBox7.TabIndex = 2;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new System.Drawing.Point(35, 64);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new System.Drawing.Size(180, 27);
+            textBox6.TabIndex = 1;
+            // 
+            // button3
+            // 
+            button3.Location = new System.Drawing.Point(482, 58);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(113, 38);
+            button3.TabIndex = 0;
+            button3.Text = "Ödünç Ver";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(textBox1);
+            tabPage2.Controls.Add(label1);
+            tabPage2.Controls.Add(button1);
+            tabPage2.Location = new System.Drawing.Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            tabPage2.Size = new System.Drawing.Size(1098, 410);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Üye Sorgula";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(59, 37);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(125, 27);
+            textBox1.TabIndex = 2;
+            textBox1.Text = "1";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(475, 39);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(0, 20);
+            label1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(210, 35);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Sorgula";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(label5);
+            tabPage1.Controls.Add(label4);
+            tabPage1.Controls.Add(label3);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(textBox4);
+            tabPage1.Controls.Add(textBox5);
+            tabPage1.Controls.Add(textBox3);
+            tabPage1.Controls.Add(textBox2);
+            tabPage1.Controls.Add(button2);
+            tabPage1.Location = new System.Drawing.Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            tabPage1.Size = new System.Drawing.Size(1098, 410);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Üye ekle";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new System.Drawing.Point(425, 27);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(60, 20);
+            label5.TabIndex = 8;
+            label5.Text = "Bölümü";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new System.Drawing.Point(294, 27);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(65, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Fakültesi";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(163, 27);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(72, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Numarası";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(32, 27);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(83, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Adı-Soyadı";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new System.Drawing.Point(294, 50);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new System.Drawing.Size(125, 27);
+            textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new System.Drawing.Point(425, 50);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new System.Drawing.Size(125, 27);
+            textBox5.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new System.Drawing.Point(163, 50);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new System.Drawing.Size(125, 27);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new System.Drawing.Point(32, 50);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(125, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(575, 48);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(94, 29);
+            button2.TabIndex = 0;
+            button2.Text = "Ekle";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Location = new System.Drawing.Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new System.Drawing.Size(1106, 443);
+            tabControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1118, 455);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            tabPage3.ResumeLayout(false);
+            tabPage3.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabControl1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabControl tabControl1;
+    }
+}
---
+++ kutuphaneotomasyonu/kutuphaneotomasyonu/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace kutuphaneotomasyonu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\kurtm\Desktop\Yeni Microsoft Access Database.accdb;";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string number = textBox1.Text;
+            if (string.IsNullOrWhiteSpace(number))
+            {
+                MessageBox.Show("Lütfen bir numara girin.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT Adi, Fakultesi, Bolumu FROM Ogrenciler WHERE number = @number";
+                    using (OleDbCommand command = new OleDbCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@number", number);
+                        using (OleDbDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                string ad = reader["Adi"].ToString();
+                                string fakulte = reader["Fakultesi"].ToString();
+                                string bolum = reader["Bolumu"].ToString();
+                                label1.Text = $"Adı: {ad}, Fakültesi: {fakulte}, Bölümü: {bolum}";
+                            }
+                            else
+                            {
+                                label1.Text = "Kayıt bulunamadı.";
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Bir hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string adi = textBox2.Text;
+            string number = textBox3.Text;
+            string fakultesi = textBox4.Text;
+            string bolumu = textBox5.Text;
+
+            if (string.IsNullOrWhiteSpace(adi) || string.IsNullOrWhiteSpace(number) || string.IsNullOrWhiteSpace(fakultesi) || string.IsNullOrWhiteSpace(bolumu))
+            {
+                MessageBox.Show("Lütfen tüm alanları doldurun.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "INSERT INTO Ogrenciler (Adi, [Number], Fakultesi, Bolumu) VALUES (@Adi, @Number, @Fakultesi, @Bolumu)";
+                    using (OleDbCommand command = new OleDbCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Adi", adi);
+                        command.Parameters.AddWithValue("@Number", number);
+                        command.Parameters.AddWithValue("@Fakultesi", fakultesi);
+                        command.Parameters.AddWithValue("@Bolumu", bolumu);
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Kayıt başarıyla eklendi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Bir hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string studentID = textBox6.Text;
+            string bookID = textBox7.Text;
+
+            if (string.IsNullOrWhiteSpace(studentID) || string.IsNullOrWhiteSpace(bookID))
+            {
+                MessageBox.Show("Lütfen öğrenci ID ve kitap ID girin.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string checkStockQuery = "SELECT StokAdedi FROM Kitaplar2 WHERE KitapID = @KitapID";
+                    using (OleDbCommand checkStockCommand = new OleDbCommand(checkStockQuery, connection))
+                    {
+                        checkStockCommand.Parameters.AddWithValue("@KitapID", bookID);
+                        int stock = (int)checkStockCommand.ExecuteScalar();
+
+                        if (stock <= 0)
+                        {
+                            MessageBox.Show("Bu kitap stokta yok!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            return;
+                        }
+
+                        string updateStockQuery = "UPDATE Kitaplar2 SET StokAdedi = StokAdedi - 1 WHERE KitapID = @KitapID";
+                        using (OleDbCommand updateStockCommand = new OleDbCommand(updateStockQuery, connection))
+                        {
+                            updateStockCommand.Parameters.AddWithValue("@KitapID", bookID);
+                            updateStockCommand.ExecuteNonQuery();
+                        }
+
+                        string loanDate = DateTime.Now.ToString("yyyy-MM-dd");
+                        string returnDate = DateTime.Now.AddDays(14).ToString("yyyy-MM-dd");
+
+                        string query = "INSERT INTO Odunc (KitapID, OgrenciID, OduncTarihi, IadeTarihi) VALUES (@KitapID, @OgrenciID, @OduncTarihi, @IadeTarihi)";
+                        using (OleDbCommand command = new OleDbCommand(query, connection))
+                        {
+                            command.Parameters.AddWithValue("@KitapID", bookID);
+                            command.Parameters.AddWithValue("@OgrenciID", studentID);
+                            command.Parameters.AddWithValue("@OduncTarihi", loanDate);
+                            command.Parameters.AddWithValue("@IadeTarihi", returnDate);
+                            command.ExecuteNonQuery();
+                        }
+
+                        MessageBox.Show("Kitap ödünç alındı.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Bir hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
